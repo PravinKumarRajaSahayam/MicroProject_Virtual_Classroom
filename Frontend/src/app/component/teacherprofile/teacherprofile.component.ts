774dabe49b0e5ed39946a463f998c534c0a729ee
PravinKumarRajaSahayam <155150894+PravinKumarRajaSahayam@users.noreply.github.com>
2024-02-08 11:40:03 +0530

    Add files via upload

---
+++ Frontend/src/app/component/teacherprofile/teacherprofile.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Teacher } from 'src/app/models/teacher';
+import { TeacherService } from 'src/app/services/teacher.service';
+
+@Component({
+  selector: 'app-teacherprofile',
+  templateUrl: './teacherprofile.component.html',
+  styleUrls: ['./teacherprofile.component.css']
+})
+export class TeacherprofileComponent implements OnInit {
+ 
+  profileDetails : Observable<Teacher[]> | undefined;
+  teacher: Teacher = new Teacher;
+  msg = ' ';
+  currRole = '';
+  loggedUser = '';
+  temp = false;
+
+  constructor(private _service: TeacherService, private activatedRoute: ActivatedRoute, private _router : Router) { }
+
+  ngOnInit(): void 
+  {
+    this.loggedUser = JSON.stringify(sessionStorage.getItem('loggedUser')|| '{}');
+    this.loggedUser = this.loggedUser.replace(/"/g, '');
+
+    this.currRole = JSON.stringify(sessionStorage.getItem('ROLE')|| '{}'); 
+    this.currRole = this.currRole.replace(/"/g, '');
+
+    $("#profilecard").show();
+    $("#profileform").hide();
+    this.getProfileDetails(this.loggedUser);
+  }
+
+  editProfile()
+  {
+    $("#profilecard").hide();
+    $("#profileform").show();
+  }
+
+  getProfileDetails(loggedUser : string)
+  {
+    this.profileDetails = this._service.getProfileDetails(this.loggedUser);
+    console.log(this.profileDetails);
+  }
+
+  updateTeacherProfile()
+  {
+    this._service.UpdateUserProfile(this.teacher).subscribe(
+      data => {
+        console.log("Teacher Profile Updated succesfully");
+        this.msg = "Profile Updated Successfully !!!";
+        $(".editbtn").hide();
+        $("#message").show();
+        this.temp = true;
+        $("#profilecard").show();
+        $("#profileform").hide();
+        setTimeout(() => {
+            this._router.navigate(['/teacherdashboard']);
+          }, 6000);
+      },
+      error => {
+        console.log("Profile Updation Failed");
+        console.log(error.error);
+        this.msg = "Profile Updation Failed !!!";
+      }
+    )
+  }
+
+}
